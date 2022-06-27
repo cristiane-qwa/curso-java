3a47a0adac71c70d47d7d73a2ffec8f58ff75b5b
cristiane-qwa <cristiane.andrade@qwasolucoes.com.br>
2022-06-27 11:35:34 -0300

    Iniciando exercicio de fixacao de interfaces

---
+++ src/main/java/interfacs_estudo01/exercicio_fixacao/Program.java
@@ -0,0 +1,25 @@
+package interfacs_estudo01.exercicio_fixacao;
+//
+//Uma empresa deseja automatizar o processamento de seus contratos. O processamento de um contrato consiste em gerar as
+//as parcelas a serem pagas para aquele contrato, com base no número de meses desejado.
+//A empresa utiliza um serviço de pagamento online para realizar o pagamento das parcelas. Os serviços de pagamento online
+//        tipicamente cobram um uro mensal, bem como uma taxa por pagamento. Por enquanto o serviço contratado pela empresa
+//é o Paypal que aplica juros simples de 1% a cada parcela, mais uma taxa de pagamento de 2%.
+//Fazer um programa para ler os dados de um contrato(número do contrato, data do contrato, e valor total do contrato).
+//Em seguida, o programa deve ler o número de meses para parcelamento do contrato, e daí gerar os registros de parcelas a
+//        serem pagas(data e valor), sendo a primeira parcela a ser paga um mês após a data do contrato, a segunda parcela
+//dois meses após o contrato e assim por diante. Mostrar os dados das parcelas na tela.
+
+//Example:
+//Enter contract data
+//Number: 8028
+//Date (dd/MM/yyyy): 2506/2018
+//Contract value: 600.00
+//Enter number of installments: 3
+//Installments:
+//25/07/2018 - 206.04
+//25/08/2018 - 208.08
+//25/09/2018 - 210.12
+
+public class Program {
+}
